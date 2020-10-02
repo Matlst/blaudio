65e6188189b493722bc7309578aca67b8eb55834
Matvey <matopen@mail.ru>
2020-10-03 01:29:31 +0500

    Update a2dp.sh

--- a2dp.sh
+++ a2dp.sh
@@ -3,5 +3,4 @@ sudo pactl set-card-profile 1  a2dp_sink
 sudo actl set-card-profile 2  a2dp_sink
 sudo pactl set-card-profile 3  a2dp_sink
 sudo pactl set-card-profile 4  a2dp_sink
-sudo pactl set-card-profile 4  a2dp_sink
 sudo service bluetooth restart
