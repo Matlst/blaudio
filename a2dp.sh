367227ccf7bccb6fb11260a780afa18a2d59ad6b
Matlst <72276912+Matlst@users.noreply.github.com>
2020-10-03 00:58:09 +0500

    Create a2dp.sh
    This script is used by me on raspbian 10 buster(debian 10 buster, respectively) to reconnect the profile in PulseAudio on a2dp.

---
+++ a2dp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo pactl set-card-profile 1  a2dp_sink
+sudo actl set-card-profile 2  a2dp_sink
+sudo pactl set-card-profile 3  a2dp_sink
+sudo pactl set-card-profile 4  a2dp_sink
+sudo pactl set-card-profile 4  a2dp_sink
+sudo service bluetooth restart
